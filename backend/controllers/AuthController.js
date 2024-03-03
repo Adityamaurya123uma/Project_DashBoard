0afdf3d555a1b79c3757872ff0527e92124331cb
Adityamaurya123uma <madityauma20@gmail.com>
2024-03-03 13:41:02 +0530

    [IMP] project_dash: Complete signup api

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -3,6 +3,8 @@ import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
+    debugger
+    console.log(req.body)
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = UserModel({username, email, password: hashedPassword});
 
