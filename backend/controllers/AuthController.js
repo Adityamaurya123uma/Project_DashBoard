14e4714936f3a13fe66294f7dd6416e322ab10c4
Adityamaurya123uma <madityauma20@gmail.com>
2024-03-03 12:33:02 +0530

    [IMP] project_dash: Added a middleware and a function to handle errors.

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,7 +1,7 @@
 import UserModel from "../models/UserModel.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = UserModel({username, email, password: hashedPassword});
@@ -10,6 +10,6 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.status(201).json({message: "User Created Successfully"});
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
     }
 };
--- backend/index.js
+++ backend/index.js
@@ -19,5 +19,17 @@ app.listen(3000, () => {
     console.log("Hey there!!!");
 })
 
+//Routes
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+// Middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error..."
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode
+    });
+});
