dce81ca29c9b9d6eb741cd46625d5831d261bdcb
Adityamaurya123uma <madityauma20@gmail.com>
2024-03-05 06:33:40 +0530

    [IMP] project_dash: added signin route and controller.

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,10 +1,10 @@
 import UserModel from "../models/UserModel.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/ErrorHandler.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
-    debugger
-    console.log(req.body)
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = UserModel({username, email, password: hashedPassword});
 
@@ -15,3 +15,20 @@ export const signup = async (req, res, next) => {
         next(error);
     }
 };
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        const validUser = await UserModel.findOne({ email });
+        if (!validUser) return next(errorHandler(404, "User not found"));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, "Wrong Credentials."));
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECERET);
+        const { password: hashedPassword, ...rest } = validUser._doc;
+        const expiryDate = new Date(Date.now(), 3600000); // 1 hour
+        res.cookie('access_token', token, { httponly: true, expires: expiryDate }).status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
+}
+
