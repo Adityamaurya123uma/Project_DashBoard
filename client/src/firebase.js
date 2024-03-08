67046492f52af1240b25bdd0f2a979188584a03e
Adityamaurya123uma <madityauma20@gmail.com>
2024-03-08 07:07:26 +0530

    [IMP] project_dash: gAuth

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDzOnAm0-9x346e3D804HYuxIjWEhTfeRM",
+  authDomain: "project-dash-b29f3.firebaseapp.com",
+  projectId: "project-dash-b29f3",
+  storageBucket: "project-dash-b29f3.appspot.com",
+  messagingSenderId: "546297445854",
+  appId: "1:546297445854:web:60bdf1c4f961569363714f"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
